2b6b4e1109b279d28de55a03db6f22e3b2df1ee0
kchiranjewee63 <kchiranjewee63@gmail.com>
2024-07-03 12:17:39 -0400

    Update token authorized services ids field to tokenAuthorizedServicesId

--- service/pkg/spire/spire.go
+++ service/pkg/spire/spire.go
@@ -7,6 +7,8 @@ import (
 	"github.com/spiffe/go-spiffe/v2/workloadapi"
 )
 
+const JWTSourceTimeout = 15 * time.Second
+
 func NewSpireJwtSource(endpointSocket string) (*workloadapi.JWTSource, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -20,7 +22,7 @@ func NewSpireJwtSource(endpointSocket string) (*workloadapi.JWTSource, error) {
 }
 
 func GetSpireJwtSource() (*workloadapi.JWTSource, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), JWTSourceTimeout)
 	defer cancel()
 
 	jwtSource, err := workloadapi.NewJWTSource(ctx)
