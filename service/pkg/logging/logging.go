adacf07bd8125f003b1405de142c129c06ec76d3
kchiranjewee63 <kchiranjewee63@gmail.com>
2024-07-31 16:30:44 -0400

    Fix linting issues

--- service/pkg/logging/logging.go
+++ service/pkg/logging/logging.go
@@ -1,74 +1,76 @@
 package logging
 
 import (
-    "os"
-    "go.uber.org/zap"
-    "go.uber.org/zap/zapcore"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var globalLogger *zap.Logger
 
 func InitLogger() error {
-    logLevel := os.Getenv("LOG_LEVEL")
-    if logLevel == "" {
-        logLevel = "info"
-    }
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "info"
+	}
 
-    config := zap.Config{
-        Encoding:         "json",
-        Level:            zap.NewAtomicLevelAt(getLogLevel(logLevel)),
-        OutputPaths:      []string{"stdout"},
-        ErrorOutputPaths: []string{"stderr"},
-        EncoderConfig:    zapcore.EncoderConfig{
-            TimeKey:        "timestamp",
-            LevelKey:       "level",
-            NameKey:        "logger",
-            CallerKey:      "caller",
-            FunctionKey:    zapcore.OmitKey,
-            MessageKey:     "message",
-            StacktraceKey:  "stacktrace",
-            LineEnding:     zapcore.DefaultLineEnding,
-            EncodeLevel:    zapcore.LowercaseLevelEncoder,
-            EncodeTime:     zapcore.ISO8601TimeEncoder,
-            EncodeDuration: zapcore.SecondsDurationEncoder,
-            EncodeCaller:   zapcore.ShortCallerEncoder,
-        },
-    }
+	config := zap.Config{
+		Encoding:         "json",
+		Level:            zap.NewAtomicLevelAt(getLogLevel(logLevel)),
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{
+			TimeKey:        "timestamp",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			FunctionKey:    zapcore.OmitKey,
+			MessageKey:     "message",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+	}
 
-    logger, err := config.Build()
-    if err != nil {
-        return err
-    }
+	logger, err := config.Build()
+	if err != nil {
+		return err
+	}
 
-    globalLogger = logger
+	globalLogger = logger
 
 	return nil
 }
 
 func GetLogger(component string) *zap.Logger {
-    if globalLogger == nil {
-        InitLogger()
-    }
-    return globalLogger.Named(component)
+	if globalLogger == nil {
+		InitLogger()
+	}
+
+	return globalLogger.Named(component)
 }
 
 func getLogLevel(level string) zapcore.Level {
-    switch level {
-    case "debug":
-        return zapcore.DebugLevel
-    case "info":
-        return zapcore.InfoLevel
-    case "warn":
-        return zapcore.WarnLevel
-    case "error":
-        return zapcore.ErrorLevel
-    default:
-        return zapcore.InfoLevel
-    }
+	switch level {
+	case "debug":
+		return zapcore.DebugLevel
+	case "info":
+		return zapcore.InfoLevel
+	case "warn":
+		return zapcore.WarnLevel
+	case "error":
+		return zapcore.ErrorLevel
+	default:
+		return zapcore.InfoLevel
+	}
 }
 
 func Sync() {
-    if globalLogger != nil {
-        globalLogger.Sync()
-    }
+	if globalLogger != nil {
+		globalLogger.Sync()
+	}
 }
