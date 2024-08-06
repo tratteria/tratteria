f3b4a56fe20b3638249278a6ca3b1aee09d60799
kchiranjewee63 <kchiranjewee63@gmail.com>
2024-08-06 19:45:01 -0400

    Implement Access Evaluation

--- service/pkg/accessevaluation/accessevaluator.go
+++ service/pkg/accessevaluation/accessevaluator.go
@@ -1,8 +1,17 @@
 package accessevaluation
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/tidwall/gjson"
+	"github.com/tratteria/tratteria/pkg/common"
+	"go.uber.org/zap"
 )
 
 type AccessEvaluationAPI struct {
@@ -23,24 +32,140 @@ type Token struct {
 type AccessEvaluator struct {
 	accessEvaluationAPI *AccessEvaluationAPI
 	httpClient          *http.Client
+	logger              *zap.Logger
 }
 
-func NewAccessEvaluator(accessEvaluationAPI *AccessEvaluationAPI, httpClient *http.Client) *AccessEvaluator {
+type accessEvaluationResponse struct {
+	Decision bool `json:"decision"`
+}
+
+func NewAccessEvaluator(accessEvaluationAPI *AccessEvaluationAPI, httpClient *http.Client, logger *zap.Logger) *AccessEvaluator {
+	tokenValue := accessEvaluationAPI.Authentication.Token.Value
+	if strings.HasPrefix(tokenValue, "${") && strings.HasSuffix(tokenValue, "}") {
+		envVarName := strings.TrimPrefix(strings.TrimSuffix(tokenValue, "}"), "${")
+
+		envValue := os.Getenv(envVarName)
+		if envValue != "" {
+			accessEvaluationAPI.Authentication.Token.Value = envValue
+		} else {
+			logger.Error("Environment variable %s not set", zap.String("env-var-name", envVarName))
+		}
+	}
+
 	return &AccessEvaluator{
 		accessEvaluationAPI: accessEvaluationAPI,
 		httpClient:          httpClient,
+		logger:              logger,
 	}
 }
 
-func (a *AccessEvaluator) Evaluate(accessEvalationRequest json.RawMessage) (bool, error) {
-	//TODO
-	return true, nil
+func (ae *AccessEvaluator) Evaluate(requestMapping map[string]interface{}, subject_token interface{}, requestDetails common.RequestDetails, requestContext map[string]interface{}, pathParameter map[string]string) (bool, error) {
+	if !ae.IsAccessEvaluationEnabled() {
+		return true, nil
+	}
+
+	inputData := map[string]interface{}{
+		"body":            requestDetails.Body,
+		"headers":         requestDetails.Headers,
+		"queryParameters": requestDetails.QueryParameters,
+		"subject_token":   subject_token,
+		"request_details": requestDetails,
+		"request_context": requestContext,
+	}
+
+	for key, value := range pathParameter {
+		inputData[key] = value
+	}
+
+	requestData, err := resolveJSONPaths(inputData, requestMapping)
+	if err != nil {
+		return false, fmt.Errorf("error resolving access request mapping: %w", err)
+	}
+
+	jsonData, err := json.Marshal(requestData)
+	if err != nil {
+		return false, fmt.Errorf("error marshalling access evaluation request: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, ae.accessEvaluationAPI.Endpoint, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return false, fmt.Errorf("error constructing access evaluation request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	if ae.accessEvaluationAPI.Authentication.Method == "Bearer" {
+		req.Header.Set("Authorization", "Bearer "+ae.accessEvaluationAPI.Authentication.Token.Value)
+	}
+
+	resp, err := ae.httpClient.Do(req)
+	if err != nil {
+		return false, fmt.Errorf("error making access evaluation request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return false, fmt.Errorf("error reading error response body from access evaluation api: %w", err)
+		}
+
+		return false, fmt.Errorf("access evaluation api request failed with non-ok status %d: %s", resp.StatusCode, string(bodyBytes))
+	}
+
+	var response accessEvaluationResponse
+
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return false, fmt.Errorf("error decoding response from the access evaluation api: %w", err)
+	}
+
+	return response.Decision, nil
 }
 
-func (a *AccessEvaluator) IsAccessEvaluationEnabled() bool {
-	if a.accessEvaluationAPI == nil {
+func (ae *AccessEvaluator) IsAccessEvaluationEnabled() bool {
+	if ae.accessEvaluationAPI == nil {
 		return false
 	}
 
-	return a.accessEvaluationAPI.EnableAccessEvaluation
+	return ae.accessEvaluationAPI.EnableAccessEvaluation
+}
+
+func resolveJSONPaths(inputData map[string]interface{}, mapping interface{}) (interface{}, error) {
+	jsonInput, err := json.Marshal(inputData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal input data to JSON: %w", err)
+	}
+
+	switch v := mapping.(type) {
+	case string:
+		if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
+			path := strings.TrimSuffix(strings.TrimPrefix(v, "${"), "}")
+
+			result := gjson.GetBytes(jsonInput, path)
+			if !result.Exists() {
+				return nil, fmt.Errorf("failed to extract value for path %s", path)
+			}
+
+			return result.Value(), nil
+		}
+
+		return v, nil
+	case map[string]interface{}:
+		resolvedMap := make(map[string]interface{})
+		for key, val := range v {
+			resolvedValue, err := resolveJSONPaths(inputData, val)
+			if err != nil {
+				return nil, fmt.Errorf("error resolving value for key %s: %v", key, err)
+			}
+
+			if resolvedValue != nil {
+				resolvedMap[key] = resolvedValue
+			}
+		}
+
+		return resolvedMap, nil
+	default:
+		return v, nil
+	}
 }
